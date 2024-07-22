b6010c90faf18c1f3e57e59c80f3e383f5bce0a8
porkieeeeeee <porkieeeeeee@gmail.com>
2024-07-22 17:15:14 +0900

    feat::MainLayout 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import MainLayout from "layouts/MainLayout";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <App />
+        <MainLayout>
+            <App />
+        </MainLayout>
     </React.StrictMode>
 );

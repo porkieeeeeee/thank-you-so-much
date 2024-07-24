6b905ce6b5883f7ab453cce932a7a5c684b63a7b
porkieeeeeee <porkieeeeeee@gmail.com>
2024-07-24 14:14:27 +0900

    fix::sdk 재설정

--- src/utils/functions/firebase.ts
+++ src/utils/functions/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_apiKey,
@@ -12,4 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getAnalytics(app);
+export const db = getFirestore(app);

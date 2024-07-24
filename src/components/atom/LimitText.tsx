d9c87e3b70f37467825e5f243d25eda07c05764f
porkieeeeeee <porkieeeeeee@gmail.com>
2024-07-24 16:00:09 +0900

    feat::LimitText 개발

---
+++ src/components/atom/LimitText.tsx
@@ -0,0 +1,25 @@
+import styled from "@emotion/styled";
+
+interface ILimitTextProps {
+    valueLength: number;
+    maxLength: number;
+}
+
+const LimitText = ({ valueLength, maxLength }: ILimitTextProps) => {
+    return (
+        <Contianer>
+            <span>{valueLength}</span> / <span>{maxLength + 1}</span>
+        </Contianer>
+    );
+};
+
+const Contianer = styled.p`
+    margin-top: 4px;
+    margin-left: auto;
+    color: #e4e4e4;
+    font-size: 16px;
+    font-weight: 400;
+    line-height: 20px;
+`;
+
+export default LimitText;

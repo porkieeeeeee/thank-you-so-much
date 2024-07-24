9b947369c94f8c6a2a47ac30d71fa22bbe3cbd09
porkieeeeeee <porkieeeeeee@gmail.com>
2024-07-24 16:28:32 +0900

    style::강조 텍스트 스타일 추가

--- src/components/atom/FormTitle.tsx
+++ src/components/atom/FormTitle.tsx
@@ -1,11 +1,18 @@
 import styled from "@emotion/styled";
+import { spawn } from "child_process";
 
 interface IFormTitleProps {
     label: string;
+    strongLabel?: string;
 }
 
-const FormTitle = ({ label }: IFormTitleProps) => {
-    return <Container>{label}</Container>;
+const FormTitle = ({ label, strongLabel = "" }: IFormTitleProps) => {
+    return (
+        <Container>
+            {strongLabel !== "" && <span>{strongLabel}</span>}
+            {label}
+        </Container>
+    );
 };
 
 const Container = styled.h2`
@@ -13,6 +20,11 @@ const Container = styled.h2`
     font-size: 24px;
     font-weight: 600;
     line-height: 28px;
+
+    span {
+        color: #f2625e;
+        font-weight: 700;
+    }
 `;
 
 export default FormTitle;
